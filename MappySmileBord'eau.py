f2b00205db9ff088d3050cb2eeaef72ad34b9aae
Lucile <llepage32@gmail.com>
2022-12-04 16:23:30 +0100

    initial commit

---
+++ MappySmileBord'eau.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 11:28:54 2022
+
+@author: Lucile Lepage
+"""
+
+import time
+import pandas as pd 
+import numpy as np
+import requests
+import folium
+from folium  import plugins
+import streamlit as st
+from streamlit_folium import st_folium
+from sqlalchemy import create_engine
+from streamlit_option_menu import option_menu
+
+st.title("MappySmileBord'eau")
+st.write('What transport is available around my position?')
+
+menu_selected = option_menu(None, ["MappySmileBord'eau","Home", "Plan your trip"], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+ 
+  
+st.image('bordeaux.gif')
+ 
